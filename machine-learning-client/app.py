17d97ebe51512198e53caf1449f414fa46d986f6
Isomorphismss <jw6699@nyu.edu>
2023-12-15 10:11:28 -0500

    test functions

--- machine-learning-client/app.py
+++ machine-learning-client/app.py
@@ -17,7 +17,6 @@ from flask_cors import CORS
 import pymongo
 import random
 from ml_client import recognition_image
-from urllib.parse import quote_plus
 
 app = Flask(__name__)
 CORS(app)  # Stop the security protection
@@ -54,52 +53,45 @@ def upload_image():
     get the uploaded image and do ML work.
     web-app's backend will call this API.
     """
-    try:
-        if "file" not in request.files:
-            print("No image file in request")
-            return jsonify({"error": "No image file"}), 400
-        random_number = random.randint(10000, 99999)  # generate unique file name
-        image_file = request.files["file"]
-        user_id = request.form.get("user_id", None)
-        if user_id:
-            filename = f"{user_id}_{random_number}.png"
-        else:
-            filename = "temp.png"
+    if "file" not in request.files:
+        print("No image file in request")
+        return jsonify({"error": "No image file"}), 400
+    random_number = random.randint(10000, 99999)  # generate unique file name
+    image_file = request.files["file"]
+    user_id = request.form.get("user_id", None)
+    if user_id:
+        filename = f"{user_id}_{random_number}.png"
+    else:
+        filename = "temp.png"
 
-        original_dir = "images_files"  # The shared folder
-        edited_dir = "edited_images_files"
-        if not os.path.exists(original_dir):
-            os.makedirs(original_dir)
+    original_dir = "images_files"  # The shared folder
+    edited_dir = "edited_images_files"
+    if not os.path.exists(original_dir):
+        os.makedirs(original_dir)
 
-        if not os.path.exists(edited_dir):
-            os.makedirs(edited_dir)
+    if not os.path.exists(edited_dir):
+        os.makedirs(edited_dir)
 
-        original_image_path = os.path.join(original_dir, filename)
-        edited_image_path = os.path.join(edited_dir, filename)
-        # Save the file in the shared folder
-        image_file.save(edited_image_path)
-        image_file.seek(0) # Set the pointer to the start of the file so it can be saved again
-        image_file.save(original_image_path)
+    original_image_path = os.path.join(original_dir, filename)
+    edited_image_path = os.path.join(edited_dir, filename)
+    # Save the file in the shared folder
+    image_file.save(edited_image_path)
+    image_file.seek(0) # Set the pointer to the start of the file so it can be saved again
+    image_file.save(original_image_path)
 
-        # This is the actual machine learning work
-        file_path = recognition_image(edited_image_path)  # recognition image
+    # This is the actual machine learning work
+    file_path = recognition_image(edited_image_path)  # recognition image
 
-        if user_id:  # If the user is logged in
-            # Store user id and file in the database
-            document = {
-                "user_id": user_id,
-                "filename": filename,
-            }
-            db.history.insert_one(document)
-        # Return recognition image
-        return send_file(file_path)
-    except Exception as e:
-        msg = traceback.format_exc()
-        print(msg)
-        return (
-            jsonify({"err_msg": msg, "filename": filename}),
-            500,
-        )
+    if user_id:  # If the user is logged in
+        # Store user id and file in the database
+        document = {
+            "user_id": user_id,
+            "filename": filename,
+        }
+        db.history.insert_one(document)
+    # Return recognition image
+    return send_file(file_path)
+
 
 
 if __name__ == "__main__":
